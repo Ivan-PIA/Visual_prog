6134a37d5aad814a4de64e5c1b8836c5b88ab3f2
Ivan-PIA <wind.vanya@gmail.com>
2023-11-05 21:27:18 +0700

    low value on onepixdistance

--- test2/material.cpp
+++ test2/material.cpp
@@ -3,15 +3,15 @@
 
 
 double Get_Glass(double freq){
-    return (2 + 0.2*freq);
+    return (2 + 0.2*freq)/10;
 }
 
 double Get_Glass_IRR(double freq){
-    return (23 + 3.0 * freq);
+    return (23 + 3.0 * freq)/10;
 }
 double Get_Concrete(double freq){
-    return (5 + 4.0 * freq);
+    return (5 + 4.0 * freq)/10;
 }
 double Get_Wood(double freq){
-    return (4.85 + 0.12 * freq);
+    return (4.85 + 0.12 * freq)/10;
 }
